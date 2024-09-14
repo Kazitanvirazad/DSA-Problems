865623bf4d19d8183dc77397d81e721ab5849200
Kazitanvirazad <tanvir.kaziazad@gmail.com>
2024-09-15 03:52:13 +0530

    sep 15 2024 commit 1

---
+++ src/main/java/com/geekforgeeks/problems/AlternatePositiveAndNegativeNumbers.java
@@ -0,0 +1,90 @@
+package com.geekforgeeks.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given an unsorted array arr containing both positive and negative numbers. Your task is to create an
+ * array of alternate positive and negative numbers without changing the relative order of positive
+ * and negative numbers.
+ * <p>
+ * Note: Array should start with a positive number and 0 (zero) should be considered a positive element.
+ * <p>
+ * Examples:
+ * <p>
+ * Input: arr[] = [9, 4, -2, -1, 5, 0, -5, -3, 2]
+ * <p>
+ * Output: 9 -2 4 -1 5 -5 0 -3 2
+ * <p>
+ * Explanation: Positive elements : [9,4,5,0,2]
+ * <p>
+ * Negative elements : [-2,-1,-5,-3]
+ * <p>
+ * As we need to maintain the relative order of postive elements and negative elements we will pick
+ * each element from the positive and negative and will store them. If any of the positive and negative
+ * numbers are completed. we will continue with the remaining signed elements.
+ * <p>
+ * The output is 9,-2,4,-1,5,-5,0,-3,2.
+ * <p>
+ * Input: arr[] = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]
+ * <p>
+ * Output: 5 -5 2 -2 4 -8 7 1 8 0
+ * <p>
+ * Explanation : Positive elements : [5,2,4,7,1,8,0]
+ * <p>
+ * Negative elements : [-5,-2,-8]
+ * <p>
+ * The output is 5, -5, 2, -2, 4, -8, 7, 1, 8, 0.
+ * <p>
+ * Expected Time Complexity: O(n)
+ * <p>
+ * Expected Auxiliary Space: O(n)
+ */
+public class AlternatePositiveAndNegativeNumbers {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(9, 4, -2, -1, 5, 0, -5, -3, 2));
+        print(list1);
+        System.out.println("After Rearrange");
+        rearrange(list1);
+        print(list1);
+        System.out.println("====================================");
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(-5, -2, 5, 2, 4, 7, 1, 8, 0, -8));
+        print(list2);
+        System.out.println("After Rearrange");
+        rearrange(list2);
+        print(list2);
+    }
+
+    public static void rearrange(ArrayList<Integer> arr) {
+        Queue<Integer> positiveQueue = new LinkedList<>();
+        Queue<Integer> negativeQueue = new LinkedList<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) < 0) {
+                negativeQueue.add(arr.get(i));
+            } else {
+                positiveQueue.add(arr.get(i));
+            }
+        }
+
+        arr.clear();
+
+        while (!positiveQueue.isEmpty() || !negativeQueue.isEmpty()) {
+            if (positiveQueue.peek() != null) {
+                arr.add(positiveQueue.poll());
+            }
+            if (negativeQueue.peek() != null) {
+                arr.add(negativeQueue.poll());
+            }
+        }
+    }
+
+    public static void print(ArrayList<Integer> list) {
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/geekforgeeks/problems/LeftViewOfBinaryTree.java
@@ -0,0 +1,70 @@
+package com.geekforgeeks.problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a Binary Tree, return its Left view. The left view of a Binary Tree is a set of nodes visible
+ * when the tree is visited from the Left side. If no left view is possible, return an empty array.
+ *
+ * Examples :
+ *
+ * Input: root[] = [1, 2, 3, 4, 5, 6, 7, N, 8]
+ *           1
+ *        /     \
+ *      2        3
+ *    /     \    /    \
+ *   4     5   6    7
+ *    \
+ *      8
+ * Output: [1, 2, 4, 8]
+ * Explanation: When we view the tree from the left side, we can only see the nodes 1, 2, 4, and 8.
+ * Input: root[] = [1, 3, N, N, 4]
+ * Output: [1, 3, 4]
+ * Input: root[] = [N]
+ * Output: []
+ * Expected Time Complexity: O(n)
+ * Expected Auxiliary Space: O(n)
+ */
+public class LeftViewOfBinaryTree {
+    public ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> elements = new ArrayList<>();
+        if (root == null) {
+            return elements;
+        }
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                Node current = queue.poll();
+
+                if (i == 0) {
+                    elements.add(current.data);
+                }
+
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
+
+        return elements;
+    }
+
+    class Node {
+        Node left, right;
+        int data;
+
+        public Node(int data) {
+            left = right = null;
+        }
+    }
+}
