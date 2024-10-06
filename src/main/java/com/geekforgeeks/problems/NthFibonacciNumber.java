36f61018fea0237f62baabeaf9bf584436e2c0b9
Kazitanvirazad <tanvir.kaziazad@gmail.com>
2024-10-06 07:07:32 +0530

    oct 06 2024 commit 2

--- src/main/java/com/geekforgeeks/problems/NthFibonacciNumber.java
+++ src/main/java/com/geekforgeeks/problems/NthFibonacciNumber.java
@@ -1,57 +1,40 @@
 package com.geekforgeeks.problems;
 
-import java.math.BigInteger;
-
 /**
- * 
- * Given a positive integer n, find the nth fibonacci number. Since the answer
- * can be very large, return the answer modulo 1000000007.
- * 
- * Example 1:
- * 
- * Input: n = 2 Output: 1 Explanation: 1 is the 2nd number of fibonacci series.
- * Example 2:
- * 
- * Input: n = 5 Output: 5 Explanation: 5 is the 5th number of fibonacci series.
- * Your Task: You dont need to read input or print anything. Complete the
- * function nthFibonacci() which takes n as input parameter and returns nth
- * fibonacci number.
- * 
- * Expected Time Complexity: O(n) Expected Auxiliary Space: O(n)
- * 
- * Constraints: 1<= n <=105
+ * Given a positive integer n, find the nth fibonacci number. Since the answer can be very large,
+ * return the answer modulo 1000000007.
  *
+ * Note: for the reference of this question take first fibonacci number to be 1.
+ *
+ * Examples :
+ *
+ * Input: n = 2
+ * Output: 1
+ * Explanation: 1 is the 2nd number of fibonacci series.
+ * Input: n = 5
+ * Output: 5
+ * Explanation: 5 is the 5th number of fibonacci series.
+ * Expected Time Complexity: O(n)
+ * Expected Auxiliary Space: O(n)
  */
 public class NthFibonacciNumber {
 
-	public static void main(String[] args) {
-		int n = 14521;
-		System.out.println(nthFibonacci(n));
+    public static void main(String[] args) {
+        int n = 14521;
+        System.out.println(nthFibonacci(n));
+    }
 
-	}
+    public static int nthFibonacci(int n) {
+        if (n < 3) {
+            return 1;
+        }
+        int n1 = 1, n2 = 1, n3 = 0, mod = 1000000007;
 
-	/**
-	 * 
-	 * Formula for finding nth fibonacci number => Xn = [φn – (1-φ)n]/√5 where, φ =
-	 * 1.618
-	 */
-	public static int nthFibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return 1;
-		}
-
-		double phi = (1 + Math.sqrt(5)) / 2; // 1.618
-
-//		double fib = Math.round((Math.pow(phi, n) - Math.pow((1 - phi), n)) / Math.sqrt(5));
-		Long longValue = Math.round((Math.pow(phi, n) - Math.pow((1 - phi), n)) / Math.sqrt(5));
-
-		System.out.println(longValue);
-		System.out.println(Long.MAX_VALUE);
-		int ans = longValue.intValue();
-		return ans;
-
-	}
+        for (int i = 2; i < n; ++i) {
+            n3 = (n1 + n2) % mod;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3 % mod;
+    }
 }
