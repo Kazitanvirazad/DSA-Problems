dbcea3f59524bb9fb073805df68abbd471677142
Kazitanvirazad <tanvir.kaziazad@gmail.com>
2025-04-23 01:22:58 +0530

    apr 23 2025 commit 3

---
+++ src/main/java/com/geekforgeeks/problems/parttwo/UniqueNumber_II.java
@@ -0,0 +1,42 @@
+package com.geekforgeeks.problems.parttwo;
+
+/**
+ * Given an array arr[] containing 2*n + 2 positive numbers, out of which 2*n numbers exist in pairs whereas
+ * the other two number occur exactly once and are distinct. Find the other two numbers.
+ * Return the answer in increasing order.
+ */
+public class UniqueNumber_II {
+    public static void main(String[] args) {
+        int[] sample1 = {1, 2, 3, 2, 1, 4};
+        int[] sample2 = {2, 1, 3, 2};
+        int[] sample3 = {2, 1, 3, 3};
+
+        int[] res = singleNum(sample3);
+        System.out.println(res[0] + " " + res[1]);
+    }
+
+    public static int[] singleNum(int[] arr) {
+        int xorVal = 0;
+        for (int i : arr) {
+            xorVal ^= i;
+        }
+
+        xorVal &= -xorVal;
+
+        int[] res = new int[2];
+
+        for (int num : arr) {
+            if ((num & xorVal) == 0) {
+                res[0] ^= num;
+            } else {
+                res[1] ^= num;
+            }
+        }
+        if (res[0] > res[1]) {
+            int temp = res[0];
+            res[0] = res[1];
+            res[1] = temp;
+        }
+        return res;
+    }
+}
